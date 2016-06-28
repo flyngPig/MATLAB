cc9ff70d7c529b896456c6ccff1ad2ffd5d4f870
amitbed <amitbed@post.bgu.ac.il>
2016-06-28 10:18:54 +0300

    Change back Motors block
    protection of end values

--- Motors.m
+++ Motors.m
@@ -2,19 +2,27 @@ classdef Motors < matlab.System
     
     methods (Access = protected)
  
-
         function stepImpl(obj,roll,pitch,yaw,throttle)
             if coder.target('Rtw')
-                 coder.cinclude('MyCopter.h'); 
-                 coder.ceval('copter.motors.set_roll',roll);
-                 coder.ceval('copter.motors.set_pitch',pitch);
-                 coder.ceval('copter.motors.set_yaw',yaw);
-                 coder.ceval('copter.motors.set_throttle',throttle);
+                coder.cinclude('MyCopter.h'); 
+                
+                if(roll>=-1 && roll <= 1)
+                    coder.ceval('copter.motors.set_roll',roll);
+                end
+                
+                if(pitch>=-1 && pitch <= 1)
+                    coder.ceval('copter.motors.set_pitch',pitch);
+                end
+                
+                if(yaw>=-1 && yaw <= 1)
+                    coder.ceval('copter.motors.set_yaw',yaw);
+                end
+                
+                if(throttle>=0 && throttle <= 1)
+                    coder.ceval('copter.motors.set_throttle',throttle);
+                end
             end
         end
-        
-        
-        
         
     end
 end
