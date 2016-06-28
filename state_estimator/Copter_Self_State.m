763461745f7b3059f4e01cbef980551e1c9f1870
amitbed <amitbed@post.bgu.ac.il>
2016-06-28 10:37:09 +0300

    Update Copter_Self_State.m

--- state_estimator/Copter_Self_State.m
+++ state_estimator/Copter_Self_State.m
@@ -1,16 +1,21 @@
 classdef Copter_Self_State < matlab.System 
 
    methods (Access = protected)
+   
        function [yaw roll pitch] = stepImpl(~)
-         yaw = 0.0;
-         yaw = coder.ceval('GET_YAW_STATE');
-
-         roll = 0.0;
-         roll = coder.ceval('GET_ROLL_STATE');
-
-         pitch = 0.0;
-         pitch = coder.ceval('GET_PITCH_STATE');
+        if coder.target('Rtw')
+            coder.cinclude('MyCopter.h');
+            yaw = 0.0;
+            yaw = coder.ceval('GET_YAW_STATE');
+   
+            roll = 0.0;
+            roll = coder.ceval('GET_ROLL_STATE');
+   
+            pitch = 0.0;
+            pitch = coder.ceval('GET_PITCH_STATE');
+         end
       end
+      
    end
 end 
 
