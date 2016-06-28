28c34c79fed414be5f4a4c66565b6885d8b35eb0
amitbed <amitbed@post.bgu.ac.il>
2016-06-28 10:35:36 +0300

    Update Roll_state.m

--- state_estimator/Roll_state.m
+++ state_estimator/Roll_state.m
@@ -1,9 +1,15 @@
 classdef Roll_state < matlab.System 
 
    methods (Access = protected)
+   
        function roll = stepImpl(~)
-         roll = 0.0;
-         roll = coder.ceval('GET_ROLL_STATE');
+          if coder.target('Rtw')
+            coder.cinclude('MyCopter.h');
+            
+            roll = 0.0;
+            roll = coder.ceval('GET_ROLL_STATE');
+         end
       end
+      
    end
 end 
