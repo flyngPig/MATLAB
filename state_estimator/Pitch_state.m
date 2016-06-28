4c5469488521cb7c7943d9fe0bba9ee4fff2a2c7
amitbed <amitbed@post.bgu.ac.il>
2016-06-28 10:21:46 +0300

    fixed Pitch_state block

--- state_estimator/Pitch_state.m
+++ state_estimator/Pitch_state.m
@@ -2,8 +2,12 @@ classdef Pitch_state < matlab.System
 
    methods (Access = protected)
        function pitch = stepImpl(~)
-         pitch = 0.0;
-         pitch = coder.ceval('GET_PITCH_STATE');
+         if coder.target('Rtw')
+            coder.cinclude('MyCopter.h');
+            
+            pitch = 0.0;
+            pitch = coder.ceval('GET_PITCH_STATE');
+         end
       end
    end
 end 
