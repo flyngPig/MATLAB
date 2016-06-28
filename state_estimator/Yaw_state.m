4650612d43b66af21fe97d041262d95024d24ca1
amitbed <amitbed@post.bgu.ac.il>
2016-06-28 10:36:30 +0300

    Update Yaw_state.m

--- state_estimator/Yaw_state.m
+++ state_estimator/Yaw_state.m
@@ -1,9 +1,15 @@
 classdef Yaw_state < matlab.System 
 
    methods (Access = protected)
+   
        function yaw = stepImpl(~)
-         yaw = 0.0;
-         yaw = coder.ceval('GET_YAW_STATE');
+          if coder.target('Rtw')
+            coder.cinclude('MyCopter.h');
+            
+            yaw = 0.0;
+            yaw = coder.ceval('GET_YAW_STATE');
+         end
       end
+      
    end
 end 
